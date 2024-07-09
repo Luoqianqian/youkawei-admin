fbd98de905109a8cc3c39267e85410308e64cc93
Qianqian <qianqian_luo95@163.com>
2024-07-09 18:02:25 +0800

    feat: 封装header组件，接收主副标题

---
+++ src/components/Header.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Typography } from 'antd';
+import { Button } from 'antd';
+import { DownloadOutlined } from '@ant-design/icons';
+
+const { Title, Text } = Typography;
+
+type propsType = {
+  title: string;
+  subTitle: string;
+};
+
+function Header({ title, subTitle }: propsType) {
+  return (
+    <div className="header">
+      <div className="titles">
+        <Title style={{ color: '#DEE4E9' }} level={3}>
+          {title}
+        </Title>
+        <Text strong style={{ color: '#347173' }}>
+          {subTitle}
+        </Text>
+      </div>
+      <div className="button">
+        <Button type="primary">
+          <DownloadOutlined />
+          <span>DOWNLOAD REPORTS</span>
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
--- src/views/dashboard/index.tsx
+++ src/views/dashboard/index.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import { Col, Row } from 'antd';
+import Header from '../../components/Header';
+import './index.css';
 
-function DashBoard() {
-  return <div>DashBoard</div>;
-}
+const DashBoard: React.FC = () => (
+  <>
+    <Header title="DASHBOARD" subTitle="welcome to your dashboard!" />
+    <Row>
+      <Col span={6}>col-6</Col>
+      <Col span={6}>col-6</Col>
+      <Col span={6}>col-6</Col>
+      <Col span={6}>col-6</Col>
+    </Row>
+    <Row>
+      <Col span={16}>col-12</Col>
+      <Col span={8}>col-12</Col>
+    </Row>
+    <Row>
+      <Col span={8}>col-8</Col>
+      <Col span={8}>col-8</Col>
+      <Col span={8}>col-8</Col>
+    </Row>
+  </>
+);
 
 export default DashBoard;
