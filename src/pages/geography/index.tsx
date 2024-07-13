24298c046fc1c9f2ee429362f3cc4387febe8ddd
Qianqian <qianqian_luo95@163.com>
2024-07-13 14:36:52 +0800

    feat: geography页面

--- src/pages/geography/index.tsx
+++ src/pages/geography/index.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import GeoChart from '../../components/GeoChart';
+import Header from '../../components/Header';
+import { mockGeographyData as data } from '../../data/mockData.js';
 
 function Geography() {
-  return <div>Geography</div>;
+  return (
+    <div>
+      <Header title="Geography Chart" subtitle="Simple Geography Chart" />
+      <div
+        style={{
+          height: '70vh',
+          backgroundColor: '#fff',
+          display: 'flex',
+          justifyContent: 'start',
+          alignItems: 'center',
+          paddingLeft: '20px',
+        }}
+      >
+        <GeoChart data={data} />
+      </div>
+    </div>
+  );
 }
 
 export default Geography;
