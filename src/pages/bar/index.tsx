28a1e835ca218ba2dbe874032ef3fc982d917803
Qianqian <qianqian_luo95@163.com>
2024-07-13 12:39:15 +0800

    将图表包改成nivo

--- src/pages/bar/index.tsx
+++ src/pages/bar/index.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import Header from '../../components/Header';
+import BarChart from '../../components/BarChart';
+import { mockBarData as data } from '../../data/mockData.js';
 
 function Bar() {
-  return <div>Bar</div>;
+  return (
+    <div>
+      <Header title="Bar Chart" subtitle="Simple Bar Chart" />
+      <div
+        style={{
+          height: '70vh',
+          backgroundColor: '#fff',
+          display: 'flex',
+          justifyContent: 'start',
+          alignItems: 'center',
+          paddingLeft: '20px',
+        }}
+      >
+        <BarChart data={data} />
+      </div>
+    </div>
+  );
 }
 
 export default Bar;
