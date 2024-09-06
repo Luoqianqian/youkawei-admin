d5b2821ef8bcd484501cba6e7ec1128e9764fae5
Qianqian <qianqian_luo95@163.com>
2024-09-06 21:22:49 +0800

    refactor: 重构布局和业务

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,19 @@
 import { Routes, Route } from 'react-router-dom';
 import SideBar from './pages/global/SideBar';
 import TopBar from './pages/global/TopBar';
-import DashBoard from './pages/dashboard';
-import Bar from './pages/bar';
-import Calendar from './pages/calendar';
-import Team from './pages/team';
-import Contacts from './pages/contacts';
-import Invoices from './pages/invoices';
-import Form from './pages/form';
-import Line from './pages/line';
-import Pie from './pages/pie';
-import FAQ from './pages/faq';
-import Geography from './pages/geography';
+import Home from './pages/Home';
 import './index.css';
 
 function App() {
   return (
     <>
       <div className="app">
-        <SideBar />
+        <TopBar />
         <main className="content">
-          <TopBar />
+          <SideBar />
           <div className="routes">
             <Routes>
-              <Route path="/" element={<DashBoard />} />
-              <Route path="/team" element={<Team />} />
-              <Route path="/contacts" element={<Contacts />} />
-              <Route path="/invoices" element={<Invoices />} />
-              <Route path="/form" element={<Form />} />
-              <Route path="/bar" element={<Bar />} />
-              <Route path="/pie" element={<Pie />} />
-              <Route path="/line" element={<Line />} />
-              <Route path="/faq" element={<FAQ />} />
-              <Route path="/calendar" element={<Calendar />} />
-              <Route path="/geography" element={<Geography />} />
+              <Route path="/" element={<Home />} />
             </Routes>
           </div>
         </main>
--- src/pages/global/TopBar.tsx
+++ src/pages/global/TopBar.tsx
@@ -1,25 +1,5 @@
-import React from 'react';
-import { Input, Space } from 'antd';
-import { MoonOutlined, SettingOutlined, UserOutlined, BellOutlined } from '@ant-design/icons';
-import './index.css';
-
-const { Search } = Input;
-
 function TopBar() {
-  const onSearch = () => {};
-  return (
-    <div className="top-bar">
-      <Search placeholder="Search" allowClear onSearch={onSearch} style={{ width: 180 }} />
-      <div className="icons">
-        <Space size={'middle'}>
-          <MoonOutlined />
-          <BellOutlined />
-          <SettingOutlined />
-          <UserOutlined />
-        </Space>
-      </div>
-    </div>
-  );
+  return <div className="top-bar">TopBar</div>;
 }
 
 export default TopBar;
